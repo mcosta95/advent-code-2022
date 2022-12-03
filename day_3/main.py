d58b499b18adc50a6da35a2c95414f6fee15a241
mcosta95 <95431729+mcosta95@users.noreply.github.com>
2022-12-03 19:27:43 +0000

    answer for day 3

---
+++ day_3/main.py
@@ -0,0 +1,34 @@
+import sys
+import string
+
+sys.path.insert(0, '.')
+
+# opening the file in read mode
+my_file = open("day_3/data.txt", "r")
+  
+# reading the file
+data = my_file.read()
+
+# prepare the data
+data_into_list = data.split('\n')
+
+final_score = []
+for one_item in data_into_list:
+    len_ = len(one_item)
+    half_len = len(one_item)/2
+
+    part_1 = one_item[:int(half_len)]
+    part_2 = one_item[int(half_len): int(len_)]
+
+    value_ = list(set(part_1) & set(part_2))[0]
+
+    alpha_lst = list(string.ascii_lowercase) + list(string.ascii_lowercase.upper())
+    score_ = alpha_lst.index(value_) + 1
+    final_score.append(score_)
+
+
+# get the answer
+result = sum(final_score)
+
+print(F"[ANSWER - PART 1] Final results: {result}")
+#print(F"[ANSWER - PART 2] Max top 3 value: {top_3}")
