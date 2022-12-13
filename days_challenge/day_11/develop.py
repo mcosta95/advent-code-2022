27e32ba3a9805e80d54593d59e839bdd81b88734
mcosta95 <95431729+mcosta95@users.noreply.github.com>
2022-12-13 20:13:58 +0000

    part 2 day 11

--- days_challenge/day_11/develop.py
+++ days_challenge/day_11/develop.py
@@ -1,36 +1,75 @@
-import pandas as pd
+import numpy as np
+import math
+import re
 
-def get_results_part_1(final_lst):
-
-    final_lst = [list(value_) for value_ in final_lst]
-    data_ = pd.DataFrame(final_lst)
-    visible = (data_.shape[0] * 4) - 4
-
-    for row_ in range(1, data_.shape[0]-1):
-        for column_ in range(1, data_.shape[0]-1):
-
-            value_ = int(data_[column_][row_])
-
-            top_ = any([int(i) < value_ for i in list(data_[column_][:row_].values)])
-            bottom_= any([int(i) < value_ for i in list(data_[column_][row_+1:].values)])
-
-            left_ = any([int(i) < value_ for i in list(data_.iloc[column_][:row_].values)])
-            right_ = any([int(i) < value_ for i in list(data_.iloc[column_][row_+1:].values)])
-
-            score_ = any([top_, bottom_, left_, right_])
-
-            if score_:
-                visible += 1
+def extra_cleaning(final_lst):
+    clean_lst = []
+    for monkey_ in final_lst:
+        monkey_fin = [re.findall(r'\d+', monkey_[value_]) for value_ in range(0, len(monkey_))]
+        clean_lst.append(monkey_fin)
     
-    return visible
+    operation_lst = [idx_[2].replace("Operation: new = ", "").strip() for idx_ in final_lst]
+    return clean_lst, operation_lst
 
 
-def get_score(final_lst, part, test):
+def isDivisible(number, divisor, monkey_):
 
-    if part == 1:
-        final_score = get_results_part_1(final_lst)
-    #else:
-    #    final_score = get_results_part_2(dict_translate_part_2, dict_win, dict_point, final_lst)
+    if number % divisor == 0:
+        return int(monkey_[4][0])
+    else:
+        return int(monkey_[5][0])
+
+
+def test_condition(new, monkey_):
+
+    # test
+    test = int(monkey_[3][0])
+
+    # condition
+    return isDivisible(new, test, monkey_)
+
+
+def get_worry_levels(nr_round, part, clean_lst, final_lst, operation_lst):
+
+    dict_items_inspect = { i : 0 for i in range(0, len(final_lst))}
+    dict_worry_levels = { i : [] for i in range(0, len(final_lst))}
+
+    for i in range(0, nr_round):
+        for idx_ in range(0, len(clean_lst)):
+            if i == 0:
+                items_lst = [int(s) for s in clean_lst[idx_][1]]
+                worry_levels = dict_worry_levels[idx_].copy() + items_lst
+                dict_worry_levels[idx_] = dict_worry_levels[idx_] + items_lst
+                dict_items_inspect[idx_] = dict_items_inspect[idx_] + len(items_lst)
+            else:
+                worry_levels = dict_worry_levels[idx_].copy()
+
+            for old in worry_levels:
+                # operation
+                new = eval(operation_lst[idx_])
+
+                if part == 1:
+                    new = math.floor(new/3)
+                else:
+                    worry_monkeys = [int(one_monkey[3][0]) for one_monkey in clean_lst]
+                    worry_level = np.prod(worry_monkeys)
+                    new = new % worry_level
+
+                condition = test_condition(new, clean_lst[idx_])
+                dict_items_inspect[condition] += 1 
+
+                dict_worry_levels[condition].append(new)
+                dict_worry_levels[idx_].remove(old)
+
+    return dict_items_inspect, dict_worry_levels
+
+
+def get_score(dict_worry_levels, dict_items_inspect, part, test):
+
+    extra_items = {key: len(value) for key, value in dict_worry_levels.items()}
+    final_score = {key: dict_items_inspect[key] - extra_items.get(key, 0) for key in dict_items_inspect}
+
+    final_score = np.prod(sorted(final_score.values(), reverse=True)[:2])
 
     print(F"[ANSWER - PART {part}] Final value: {final_score}")
 
@@ -40,6 +79,6 @@ def get_score(final_lst, part, test):
 
 def test_results(part, final_score):
     if part == 1:
-        assert final_score == 21
+        assert final_score == 10605
     else:
-        assert final_score == 12
+        assert final_score == 2713310158
--- days_challenge/day_11/main.py
+++ days_challenge/day_11/main.py
@@ -10,25 +10,13 @@ import math
 sys.path.insert(0, FEATURE_DIR)
 
 from utils import read_data
-from day_11.develop import get_score
+from day_11.develop import extra_cleaning, get_worry_levels, get_score
 
 # parameters
 DAY_NR = 11
-PART = 2
-TEST = False
-
-def isDivisible(number, divisor, monkey_):
-    if number % divisor == 0:
-        return int(monkey_[4][0])
-    else:
-        return int(monkey_[5][0])
-
-def test_condition(monkey_):
-    # test
-    test = int(monkey_[3][0])
-
-    # condition
-    return isDivisible(new, test, monkey_)
+PART = 1
+TEST = True
+NR_ROUND = 20
 
 
 if __name__ == '__main__':
@@ -41,48 +29,12 @@ if __name__ == '__main__':
     # import data
     final_lst = read_data(folder=f"days_challenge/day_{DAY_NR}/data", filename=filename_, split="\n\n", extra_clean="\n")
 
-    clean_lst = []
-    for monkey_ in final_lst:
-        monkey_fin = [re.findall(r'\d+', monkey_[value_]) for value_ in range(0, len(monkey_))]
-        clean_lst.append(monkey_fin)
-    
-    operation_lst = [idx_[2].replace("Operation: new = ", "").strip() for idx_ in final_lst]
+    # extra cleaning
+    clean_lst, operation_lst = extra_cleaning(final_lst)
 
-    dict_items_inspect = { i : 0 for i in range(0, len(final_lst))}
-    dict_next_round = { i : [] for i in range(0, len(final_lst))}
+    # development
+    dict_items_inspect, dict_worry_levels = get_worry_levels(NR_ROUND, PART, clean_lst, final_lst, operation_lst)
 
-    for idx_ in range(0, len(clean_lst)):
-        items_lst = [int(s) for s in clean_lst[idx_][1]]
-        iterator_lst = dict_next_round[idx_].copy() + items_lst
-        dict_next_round[idx_] = dict_next_round[idx_] + items_lst
-        dict_items_inspect[idx_] = dict_items_inspect[idx_] + len(items_lst)
+    # score
+    get_score(dict_worry_levels, dict_items_inspect, PART, TEST)
 
-    # round
-    nr_round = 10000
-
-    i = 0
-    while i <= nr_round:
-        for idx_ in range(0, len(clean_lst)):
-
-            for old in dict_next_round[idx_].copy():
-                # operation
-                new = eval(operation_lst[idx_])
-
-                #if PART==1:
-                #    new = math.floor(new/3)
-
-                condition = test_condition(clean_lst[idx_])
-                dict_items_inspect[condition] += 1 
-
-                dict_next_round[condition].append(new)
-                dict_next_round[idx_].remove(old)
-        i += 1
-
-
-    extra_items = {key: len(value) for key, value in dict_next_round.items()}
-    final_score = {key: dict_items_inspect[key] - extra_items.get(key, 0) for key in dict_items_inspect}
-
-    final_result = np.prod(sorted(final_score.values(), reverse=True)[:2])
-
-    print(final_score)
-    print(final_result)
